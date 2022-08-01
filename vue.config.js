0fbfd5f6387557414508e63e4f049b1cdb4bfe44
Aubrey-Xu <janec3433@gmail.com>
2022-08-01 14:43:18 +0800

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,37 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  chainWebpack: (config) => {
+    // 发布模式
+    config.when(process.env.NODE_ENV === 'production', (config) => {
+      config.entry('app').clear().add('./src/main-prod.js')
+      config.set('externals', {
+        vue: 'Vue',
+        'vue-router': 'VueRouter',
+        axios: 'axios',
+        nprogress: 'NProgress',
+        'vue-lazyload': 'VueLazyload'
+      })
+      config.plugin('html').tap((args) => {
+        args[0].isProd = true
+        return args
+      })
+    })
+    // 开发模式
+    config.when(process.env.NODE_ENV === 'development', (config) => {
+      config.entry('app').clear().add('./src/main-dev.js')
+      config.plugin('html').tap((args) => {
+        args[0].isProd = false
+        return args
+      })
+    })
+  },
   devServer: {
     proxy: {
-      '/api': { 
-        target: 'https://lianghj.top:3000', 
+      '/api': {
+        target: 'https://lianghj.top:3000',
         changeOrigin: true, // 允许跨域
-        pathRewrite: { 
+        pathRewrite: {
           '^/api': ''
         }
       }
